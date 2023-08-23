5fb7ee2a6f4cea20f43bbe65b861051ece043185
AvishayP <peravishaymoshe@gmail.com>
2023-08-23 12:16:52 +0300

    adding ex2.ts

--- ex2.ts
+++ ex2.ts
@@ -1,4 +1,3 @@
-
 // בנו תכנית שמדמה את המשחק אבן נייר ומספריים
 // התכנית תגדיר מי המנצח לפי חוקיות המשחק
 // אבן מנצח מספריים שמנצחות נייר שמנצח אבן
@@ -7,18 +6,37 @@
 // שחקן  יכול לבחור בין אבן נייר ומספרים בלבד! אך שחקן יכול שלא תהיה לו בחירה
 // התכנית יכולה להחזיר את המחרוזות
 // 'tie', 'player1', 'player2' 
+
 // התשובה חייבת להכיל
 // ENUM
 // type/interface
 // union
+
 // במידה ולשחקן אין בחירה הצג זרקו שגיאה
 // ממשו את הפונקציה הוסיפו טיפוסים לפרמטרים ולערך החזרה של הפונקציה
-
-
-
-function playGame(player1, player2) {
-
+enum Choice {
+    Paper,
+    Rock,
+    Scissors
 }
+type Player = Choice | undefined;
+function playGame(player1: Player, player2: Player): string {
+    if (typeof player1 === 'undefined' || typeof player2 === 'undefined') {
+        return 'Throw Error: No move insert';
+    } else {
+        if (player1 === player2) {
+            return 'tie';
+        }
+        if (player1 == Choice.Paper && player2 == Choice.Rock ||
+            player1 == Choice.Rock && player2 == Choice.Scissors ||
+            player1 == Choice.Scissors && player2 == Choice.Paper) {
+            return 'player1';
+        } else {
+            return 'player2';
+        }
+    }
+}
+let player1 = Choice.Paper, player2 = Choice.Scissors;
 
 const play = playGame(player1, player2);
 console.log(play);
